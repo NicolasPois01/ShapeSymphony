9a66b0fa6e2f819032217b35326dcba7760c2e04
eliot <contact@eliotmasset.fr>
2023-09-22 23:39:38 +0200

    update: a lot of little features/fix

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -11,8 +11,10 @@ import { Component, HostListener } from '@angular/core';
 export class MainComponentComponent {
 
   grid: boolean = false;
+  precisionMode: boolean = false;
 
   handleKeyboardEvent(event: KeyboardEvent) {
     if(event.key === "Shift") this.grid = !this.grid;
+    else if(event.key === "Control") this.precisionMode = !this.precisionMode;
   }
 }
--- src/app/square/circle/circle.component.ts
+++ src/app/square/circle/circle.component.ts
@@ -8,6 +8,4 @@ import {Circle} from "../../models/circle";
 })
 export class CircleComponent {
   @Input() circle?: Circle;
-
-  
 }
