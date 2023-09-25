9f1ca9ffa4d59bd0c97519de3fd5848f091e48b3
NicolasPois01 <pois.nicolas01@gmail.com>
2023-09-25 12:08:10 +0200

    .

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -12,34 +12,42 @@ export class TimerComponent {
   milliseconds: number = 0;
   timer: any;
   isRunning: boolean = false;
+  elapsedTime = 0; // total time that has elapsed in milliseconds
+  startTime: any;
 
   constructor(private timerService: TimerService) {
   }
 
   startTimer(): void {
-    this.isRunning = true;
-    this.timerService.start();
-    if (!this.timer) {
-      this.timer = setInterval(() => {
-        this.milliseconds += 1;
-        if (this.milliseconds >= 100) {
-          this.seconds++;
-          this.milliseconds = 0;
-        }
-        if (this.seconds >= 60) {
-          this.minutes++;
-          this.seconds = 0;
-        }
-      }, 1);
+    if (!this.isRunning) {
+      this.isRunning = true;
+
+      // Adjust the start time based on previously elapsed time
+      this.startTime = Date.now() - this.elapsedTime;
+
+      this.timerService.start();
+
+      if (!this.timer) {
+        this.timer = setInterval(() => {
+          this.elapsedTime = Date.now() - this.startTime;
+
+          this.milliseconds = this.elapsedTime % 1000;
+          this.seconds = Math.floor(this.elapsedTime / 1000) % 60;
+          this.minutes = Math.floor(this.elapsedTime / 60000);
+        }, 10);
+      }
     }
   }
 
   pauseTimer(): void {
-    this.isRunning = false;
-    this.timerService.pause();
-    if (this.timer) {
-      clearInterval(this.timer);
-      this.timer = null;
+    if (this.isRunning) {
+      this.isRunning = false;
+      this.timerService.pause();
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
     }
   }
+
 }
