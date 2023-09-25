fb3b57d6bafd107062e4366d8f4c5a4a6d533a64
TomMuet <tom.muet@mines-ales.org>
2023-09-25 09:51:59 +0200

    ajout import/export et octave et alteration

---
+++ src/app/octave/octave.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { SoundService } from '../services/sound.service';
+
+@Component({
+  selector: 'app-octave',
+  templateUrl: './octave.component.html',
+  styleUrls: ['./octave.component.scss']
+})
+export class OctaveComponent {
+  @Output() octaveChanged = new EventEmitter<void>();
+  selectedOctave: number = this.soundService.getActiveOctave();
+
+  constructor(private soundService: SoundService) {}
+
+  updateOctave(): void {
+    this.soundService.setActiveOctave(this.selectedOctave);
+    this.octaveChanged.emit();
+  }
+}
--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -1,18 +1,74 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoundService {
-
+  selectionChanged = new EventEmitter<void>();
   instruments = ["Piano", "Batterie", "Guitare", "Violon", "Trompette", "Clavecin"];
-  notes = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"]
+  notes = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"];
 
-  activeInstrument = "Piano"
-  activeNote = "Do"
+  activeInstrument: string = "Piano";
+  activeNote: string = "Do";
+  activeOctave: number = 3;
+  activeAlteration: number = 0;
+
   constructor() { }
 
   setActiveInstrument(instrument: string) {
     this.activeInstrument = instrument;
+    this.selectionChanged.emit();
   }
+
+
+  getActiveInstrument(): string {
+    return this.activeInstrument;
+  }
+
+  setActiveNote(note: string) {
+    this.activeNote = note;
+    this.selectionChanged.emit();
+  }
+
+  getActiveNote(): string {
+    return this.activeNote;
+  }
+
+  setActiveOctave(octave: number) {
+    this.activeOctave = octave;
+    this.selectionChanged.emit();
+  }
+
+  getActiveOctave(): number {
+    return this.activeOctave;
+  }
+
+  setActiveAlteration(alteration: number) {
+    this.activeAlteration = alteration;
+    this.selectionChanged.emit();
+  }
+
+  getActiveAlteration(): number {
+    return this.activeAlteration;
+  }
+
+  getCurrentSelection(): string {
+    let alterationSymbol = '';
+    // @ts-ignore
+    switch (this.activeAlteration) {
+      case -1:
+        alterationSymbol = '♭';
+        break;
+      case 1:
+        alterationSymbol = '♯';
+        break;
+      default:
+        alterationSymbol = '';
+        break;
+    }
+    return `${this.activeInstrument} ${this.activeNote}${alterationSymbol}${this.activeOctave}`;
+
+  }
+
+
 }
