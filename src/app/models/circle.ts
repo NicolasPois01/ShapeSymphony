eb61c0f805f08c133644acb6ca98562cd52c3e39
GabrielCompan <gabrielcompanpro@gmail.com>
2023-11-29 09:50:58 +0100

    Uptade Cicle modèle

--- src/app/models/circle.ts
+++ src/app/models/circle.ts
@@ -1,76 +1,20 @@
 export interface Circle {
-  id: number,
-  x: number,
-  y: number,
-  xSpeed: number,
-  ySpeed: number,
-  color: string,
-  startX: number,
-  startY: number,
-  instrument: string,
-  percussion:string,
-  note: string,
-  alteration: string,
-  octave: number,
-  volume: number,
-  spawnTime: number,
-  maxBounces: number,
-  maxTime: number
-  isColliding: boolean;
-
-
-
-export class Circle {
-  public id: number;
-  public x: number;
-  public y: number;
-  public xSpeed: number;
-  public ySpeed: number;
-  public color: string;
-  public startX: number;
-  public startY: number;
-  public instrument: string;
-  public percussion: string;
-  public note: string;
-  public alteration: string;
-  public octave: number;
-  public spawnTime: number | null;
-  public maxBounces: number | null;
-  public maxTime: number | null;
-  public isColliding: boolean = false;
-  public contactPoint: { x: number; y: number } | null = null;
-
-  constructor(
     id: number,
-    startX: number,
-    startY: number,
+    x: number,
+    y: number,
     xSpeed: number,
     ySpeed: number,
     color: string,
+    startX: number,
+    startY: number,
     instrument: string,
     percussion: string,
     note: string,
     alteration: string,
     octave: number,
-    spawnTime: number | null = null,
-    maxBounces: number | null = null,
-    maxTime: number | null = null
-  ) {
-    this.id = id;
-    this.color = color;
-    this.x = startX;
-    this.y = startY;
-    this.startX = startX;
-    this.startY = startY;
-    this.xSpeed = xSpeed;
-    this.ySpeed = ySpeed;
-    this.instrument = instrument;
-    this.percussion = percussion;
-    this.note = note;
-    this.alteration = alteration;
-    this.octave = octave;
-    this.spawnTime = spawnTime;
-    this.maxBounces = maxBounces;
-    this.maxTime = maxTime;
-  }
+    volume: number,
+    spawnTime: number,
+    maxBounces: number,
+    maxTime: number
+    isColliding: boolean;
 }
