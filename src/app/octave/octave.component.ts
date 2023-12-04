1341767523538f91318a4563153cacef15aaf054
NicolasPois01 <pois.nicolas01@gmail.com>
2023-12-04 16:11:56 +0100

    octave limit (bass & xylo)

--- src/app/octave/octave.component.ts
+++ src/app/octave/octave.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { SoundService } from '../services/sound.service';
 
 @Component({
@@ -7,20 +7,30 @@ import { SoundService } from '../services/sound.service';
   styleUrls: ['./octave.component.scss']
 })
 export class OctaveComponent {
-  @Output() octaveChanged = new EventEmitter<void>();
   selectedOctave!: number;
+  minOctave: number = 1;
+  maxOctave: number = 7;
 
   constructor(private soundService: SoundService) {
     this.soundService.activeOctave$.subscribe(octave => this.selectedOctave = octave);
+    this.soundService.activeInstrument$.subscribe(instru => {
+      if (instru == "Xylophone" || instru == "Bass") {
+        this.minOctave = 4;
+      }
+      else {
+        this.minOctave = 1;
+      }
+      if (this.selectedOctave < this.minOctave) this.soundService.setActiveOctave(this.minOctave);
+    });
   }
   increaseOctave() {
-    if (this.selectedOctave < 7) {
+    if (this.selectedOctave < this.maxOctave) {
       this.soundService.setActiveOctave(this.selectedOctave + 1);
     }
   }
 
   decreaseOctave() {
-    if (this.selectedOctave > 1) {
+    if (this.selectedOctave > this.minOctave) {
       this.soundService.setActiveOctave(this.selectedOctave - 1);
     }
   }
